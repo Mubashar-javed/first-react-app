6c4c3be7bf06449819545970589dd9b2c57379e4
Mubashar Javed <mubasharbravo302@gmail.com>
2020-09-30 17:29:04 +0500

    useReducer hook added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,36 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Example from "./Example";
 import List from './List'
 
+const initialStories = [
+    {
+        title: "React is cool",
+        url: "https://reactjs.org",
+        released: 2013,
+        objId: 0,
+    },
+    {
+        title: "Django is just django",
+        url: "https://djangoproject.com",
+        released: 2005,
+        objId: 1,
+    },
+    {
+        title: "Google is best",
+        url: "https://www.google.com",
+        released: 1997,
+        objId: 2,
+    },
+];
+
+const getAsyncStories = () => {
+    // delaying for 2 secs for demo purpose
+    return new Promise(resolve =>
+        setTimeout(() =>
+                resolve({data: {stories: initialStories}}),
+            2000)
+    );
+};
+
 // we use `use` prefix to define custom hooks in react, its a coding convention.
 const useSemiPersistentState = (key, initialValue = '') => {
     const [value, setValue] = React.useState(
@@ -12,51 +42,69 @@ const useSemiPersistentState = (key, initialValue = '') => {
             localStorage.setItem(key, value);
         }, [value, key] // only re-run if the value or key changed
     );
-
     // its a React convention to return an array for custom hooks
     return [value, setValue];
 };
 
+const storiesReducer = (state, action) => {
+    switch (action.type) {
+        case "SET_STORIES":
+            return action.payload;
+        case "REMOVE_STORIES":
+            return state.filter(story => action.payload.objId !== story.objId);
+        default:
+            throw  new Error();
+    }
+
+}
+
 function App() {
-    const stories = [
-        {
-            title: "React is cool",
-            url: "https://reactjs.org",
-            released: 2013,
-            objId: 0,
-        },
-        {
-            title: "Django is just django",
-            url: "https://djangoproject.com",
-            released: 2005,
-            objId: 1,
-        },
-        {
-            title: "Google is best",
-            url: "https://www.google.com",
-            released: 1997,
-            objId: 2,
-        },
-    ];
+
 
     const [searchTerm, setSearchTerm] = useSemiPersistentState("search");
 
+    //it mean initial value of stories=[]
+    // const [stories, setStories] = React.useState([]);
+    const [stories, dispatchStories] = React.useReducer(storiesReducer, []);
+
+    const [isLoading, setIsLoading] = React.useState(false);
+    const [isError, setIsError] = React.useState(false);
+
+    React.useEffect(() => {
+        setIsLoading(true);
+        getAsyncStories().then(result => {
+            dispatchStories({
+                type: "SET_STORIES",
+                payload: result.data.stories
+            });
+            setIsLoading(false);
+        }).catch(() => setIsError(true))
+    }, []);
+
+    const handleRemoveStory = item => {
+        // const newStories = stories.filter(story => item.objId !== story.objId);
+        dispatchStories({
+            type: "REMOVE_STORIES",
+            payload: item
+        });
+    };
 
     const handleSearch = event => {
         setSearchTerm(event.target.value);
     };
 
     const searchedStories = stories.filter(story => {
-        const {title, released, url} = story;
+        const {title} = story;
         const term = searchTerm.toLowerCase();
-        return title.includes(term) ||
-            url.includes(term) ||
-            released.toString().includes(term);
+        return title.includes(term);
     });
-
+    const loadingSpinner = <div className="text-center">
+        <div className="spinner-border text-primary" role="status">
+            <span className="sr-only">Loading...</span>
+        </div>
+    </div>
     return (
         <>
-            {/*Adding onSearch props here which will call handleSearch function every time.*/}
             <InputWithLabel
                 id="search"
                 value={searchTerm}
@@ -67,8 +115,10 @@ function App() {
             </InputWithLabel>
 
             <h2>My List</h2>
-            {/* we are passing our stories as a list in our List component */}
-            <List list={searchedStories}/>
+            {isError && <p>Something went wrong......</p>}
+            {isLoading ? loadingSpinner :
+                <List list={searchedStories} onRemoveItem={handleRemoveStory}/>
+            }
             <hr/>
             <Example/>
         </>
@@ -77,7 +127,10 @@ function App() {
 
 // This component will just render an Input with a label
 const InputWithLabel = ({id, children, onInputChange, value, type = 'text', isFocused = false}) => {
+
+
     const inputRef = React.useRef();
+
 
     React.useEffect(() => {
             if (isFocused && inputRef.current) {
@@ -99,5 +152,6 @@ const InputWithLabel = ({id, children, onInputChange, value, type = 'text', isFo
         </>
     );
 };
+
 
 export default App;
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,19 +1,27 @@
 import React from 'react';
 
 
-const List = ({list}) => {
-    return (
-        <>
-            {list.map((item) => (
-                <div key={item.objId}>
-                    <a href={item.url}>{item.title}</a>
-                    <p>{item.url}</p>
-                    <p>{item.released}</p>
-                    <hr/>
-                </div>
-            ))}
-        </>
+const List = ({list, onRemoveItem}) => {
+    return list.map(item => {
+            return <Item key={item.objId} item={item} onRemoveItem={onRemoveItem}/>;
+        }
     );
 };
 
+const Item = ({item, onRemoveItem}) => {
+    return (
+        <div>
+          <span>
+              <a href={item.url}>{item.title}</a>
+          </span>
+            <p>{item.released}</p>
+            <p>{item.url}</p>
+            <button className="btn btn-danger btn-sm"
+                    onClick={() => onRemoveItem(item)}>
+                Dismiss!
+            </button>
+            <hr/>
+        </div>
+    );
+};
 export default List;
